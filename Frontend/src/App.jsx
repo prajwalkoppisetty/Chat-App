f2afbc07829de8c1a06689beef35bcfcaf2cd4b8
Prajwal <prajwalkoppisetty@gmail.com>
2024-11-15 15:02:07 +0530

    modified:   Frontend/package-lock.json 	modified:   Frontend/package.json 	modified:   Frontend/src/App.jsx 	new file:   package-lock.json

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,13 +4,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Signup from './../Components/Signup';
 import Login from './../Components/Login';
-
+import HomePage from '../Components/HomePage';
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/" element={<HomePage />} />
       </Routes>
     </Router>
   );
